ef099a827f2dbdc68d21fcdc269aceff4723fdf5
javier243025 <fjromero10@utpl.edu.ec>
2024-05-22 11:18:05 -0500

    Mi primer ejemplo

---
+++ actividad-suplementaria/centros_comerciales.py
@@ -0,0 +1,10 @@
+class CentrosComerciales:
+    
+    def __init__(self, nombre, direccion, numero_tiendas, capacidad_parqueo):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.numero_tiendas = numero_tiendas
+        self.capacidad_parqueo = capacidad_parqueo
+
+def __str__(self):
+        return f"{self.nombre} ubicado en {self.direccion} con {self.numero_tiendas} tiendas y capacidad de parqueo para {self.capacidad_parqueo} autos."
